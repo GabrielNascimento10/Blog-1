b6402ed55e7d3d0eef5c33c037cbcd2bafc41a69
TatianaCampos10 <60801932+TatianaCampos10@users.noreply.github.com>
2020-02-14 22:27:40 -0300

    Adicionando primeiros arquivos do Model

---
+++ Blog/Models/Blog/Postagem/PostagemEntity.cs
@@ -0,0 +1,19 @@
+﻿using Blog.Models.Blog.Autor;
+using Blog.Models.Blog.Categoria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models.Blog.Postagem
+{
+    public class PostagemEntity
+    {
+        public String Titulo;
+        public AutorEntity Autor;
+        public CategoriaEntity Categoria;
+    
+
+
+    }
+}
