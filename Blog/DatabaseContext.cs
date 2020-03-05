2c92635ff03c3cc7c5ed02a59c077dde4aae507d
GabrielNascimento10 <60801918+GabrielNascimento10@users.noreply.github.com>
2020-03-05 20:39:43 -0300

    BD

---
+++ Blog/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Blog.Models.Blog.Categoria;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<CategoriaEntity> categoria { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            base.OnConfiguring(optionBuilder);
+
+            optionBuilder.UseMySql("Server=localhost;User=root;password=root;Database=blog");
+        }
+    }
+}
