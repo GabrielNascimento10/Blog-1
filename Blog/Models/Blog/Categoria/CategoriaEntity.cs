cf9ae31ad0dfadef8f5852c607d34ab3b191f92d
TatianaCampos10 <60801932+TatianaCampos10@users.noreply.github.com>
2020-03-05 20:40:23 -0300

    Banco&Entidades

--- Blog/Models/Blog/Categoria/CategoriaEntity.cs
+++ Blog/Models/Blog/Categoria/CategoriaEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,11 @@ namespace Blog.Models.Blog.Categoria
 {
     public class CategoriaEntity
     {
-        public string Nome;
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string Nome { get; set; }
     }
 }
